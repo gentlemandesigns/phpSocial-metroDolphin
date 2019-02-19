86f4dd0a9160035869a95ff804fa9bcd878e4c5f
GentlemanDesigns <gentlemandesigns@outlook.com>
2019-02-19 15:12:22 +0200

    Compatibility with phpSocial 5.x

---
+++ themes/metroDolphin/info.php
@@ -0,0 +1,13 @@
+<?php
+// Theme Name
+$name = 'metroDolphin';
+
+// Theme Author
+$author = 'GentlemanDesigns';
+
+// Theme URL
+$url = 'http://codecanyon.net/user/gentlemandesigns';
+
+// Theme Version
+$version = '5.2';
+?>
