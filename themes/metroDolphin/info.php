bdb30389080a24a8db9c1406aa6b0db6bffe1027
GentlemanDesigns <gentlemandesigns@outlook.com>
2019-02-27 15:42:22 +0200

    Theme URL Fix

--- themes/metroDolphin/info.php
+++ themes/metroDolphin/info.php
@@ -6,7 +6,7 @@ $name = 'metroDolphin';
 $author = 'GentlemanDesigns';
 
 // Theme URL
-$url = 'http://codecanyon.net/user/gentlemandesigns';
+$url = 'https://github.com/gentlemandesigns/metroDolphin';
 
 // Theme Version
 $version = '5.2';
